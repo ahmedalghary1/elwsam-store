9ca18dd31808a368a3c43044e2913b25eae66997
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-03-30 15:14:42 +0200

    update

--- products/models.py
+++ products/models.py
@@ -60,6 +60,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
     class Meta:
         ordering = ['order']
         verbose_name = 'منتج'
