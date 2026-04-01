5e61b35456ff73df2f7317d09e891a4b48ae45d1
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-04-01 08:33:03 +0200

    update

--- project/settings.py
+++ project/settings.py
@@ -1,15 +1,13 @@
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-import os
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-rl17&5ecdx1h131wc@wy!^r-*7zh(q@!+#4jy2*%tw=z@4dv2i')
 
@@ -80,26 +78,25 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'elwsamst_shop',
-#         'USER': 'elwsamst_usershop',
-#         'PASSWORD': 'elwsam@100',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elwsamst_shop',
+        'USER': 'elwsamst_usershop',
+        'PASSWORD': 'elwsam@100',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
